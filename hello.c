e1cb6d79b6da4b24b8ae1651e5cf9e83f4179ec3
seriousColb <cmar14.cm@gmail.com>
2025-10-27 14:26:12 -0400

    Repo made and first upload done

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    printf("Hello teach. My name is Colby John Martin and I am in your CMPE311 class.\n");
+    return 0;
+}
